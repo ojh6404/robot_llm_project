233cafd695e6f42ee1bf5e2ba9b9b53f98a9b9ed
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-04-12 21:17:18 +0900

    update

--- llm_ros/node_scripts/perception_node.py
+++ llm_ros/node_scripts/perception_node.py
@@ -1,60 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
 import rospy
 import dynamic_reconfigure.client
-from llm_common_msgs.msg import Query, Message
-from std_srvs.srv import Empty, EmptyResponse
-from jsk_recognition_msgs.msg import ClassificationResult
-from jsk_recognition_msgs.msg import RectArray
-
-"""
-1. ask vlm what object is in interaction
-2. ask gd to detect the object
-"""
+from llm_common_msgs.srv import PerceptionQuery, PerceptionQueryResponse
 
 
 class PerceptionNode(object):
     def __init__(self):
         super(PerceptionNode, self).__init__()
-        self.gd_client = dynamic_reconfigure.client.Client("/grounding_dino_node")
-        self.vqa_client = dynamic_reconfigure.client.Client("/vqa_node")
+        self.dino_node = rospy.get_param("~dino_node", "/grounding_dino_node")
+        self.yolo_node = rospy.get_param("~yolo_node", "/yolo_node")
+        self.vlpart_node = rospy.get_param("~vlpart_node", "/vlpart_reconfigure_node")
+        self.vqa_node = rospy.get_param("~vqa_node", "/vqa_node")
 
-        self.sub_cmd = rospy.Subscriber(
-            "/llm_agent/perception/query", Query, self.callback, queue_size=1
-        )
-        self.pub_detection = rospy.Publisher(
-            "/llm_agent/perception/detection", Message, queue_size=1
+        self.perception_query_service = rospy.Service(
+            "/perception_node/query", PerceptionQuery, self.perception_query_service_cb
         )
 
-    def query_gd(self, obj_to_query):
-        self.gd_config = self.gd_client.get_configuration()
-        self.gd_config["classes"] = obj_to_query
-        self.gd_client.update_configuration(self.gd_config)
-        rospy.sleep(2.0)
-        gd_answer = rospy.wait_for_message(
-            "/grounding_dino_node/output/class", ClassificationResult
-        )
-        rect = rospy.wait_for_message("/grounding_dino_node/output/rects", RectArray)
-        print(gd_answer)
-        print(rect)
-
-    def callback(self, msg):
-        vqa_answer = rospy.wait_for_message("/vqa_node/output/text_gen", Message)
-        object_detection_query = vqa_answer.message.lower()
-        rospy.loginfo("VQA answer: %s", object_detection_query)
-        self.gd_config = self.gd_client.get_configuration()
-        self.gd_config["classes"] = object_detection_query
-        self.gd_client.update_configuration(self.gd_config)
-        rospy.sleep(2.0)
-        gd_answer = rospy.wait_for_message(
-            "/grounding_dino_node/output/class", ClassificationResult
-        )
-        rect = rospy.wait_for_message("/grounding_dino_node/output/rects", RectArray)
-        print(gd_answer)
-        print(rect)
+    def perception_query_service_cb(self, req):
+        detector = req.detector
+        detection_query = req.query
+        if detector == "grounding_dino":
+            self.gd_client = dynamic_reconfigure.client.Client(self.dino_node)
+            self.gd_client.update_configuration({"classes": detection_query})
+        elif detector == "yolo":
+            self.yolo_client = dynamic_reconfigure.client.Client(self.yolo_node)
+            self.yolo_client.update_configuration({"classes": detection_query})
+        elif detector == "vlpart":
+            self.vlpart_client = dynamic_reconfigure.client.Client(self.vlpart_node)
+            self.vlpart_client.update_configuration({"classes": detection_query})
+        elif detector == "vqa":
+            self.vqa_client = dynamic_reconfigure.client.Client(self.vqa_node)
+            self.vqa_client.update_configuration({"question": detection_query})
+        else:
+            rospy.logerr("Invalid detector: {}".format(detector))
+            return PerceptionQueryResponse(result=False)
+        return PerceptionQueryResponse(result=True)
 
 
 if __name__ == "__main__":
     rospy.init_node("perception_node")
+    rospy.loginfo("Starting perception node")
     node = PerceptionNode()
     rospy.spin()
