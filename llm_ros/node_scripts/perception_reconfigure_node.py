1ef706f9c257e6246792e63a32b6020877e44727
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-04-15 17:03:31 +0900

    update

--- llm_ros/node_scripts/perception_reconfigure_node.py
+++ llm_ros/node_scripts/perception_reconfigure_node.py
@@ -3,7 +3,10 @@
 
 import rospy
 import dynamic_reconfigure.client
+from std_srvs.srv import Empty
+from sensor_msgs.msg import Image
 from llm_common_msgs.srv import PerceptionReconfigure, PerceptionReconfigureResponse
+from tracking_ros_utils.srv import CutiePrompt, CutiePromptRequest
 
 
 class PerceptionReconfigureNode(object):
@@ -14,10 +17,15 @@ class PerceptionReconfigureNode(object):
         self.vlpart_node = rospy.get_param("~vlpart_node", "/vlpart_reconfigure_node")
         self.clip_node = rospy.get_param("~clip_node", "/clip_inference_node")
         self.vqa_node = rospy.get_param("~vqa_node", "/vqa_node")
+        self.cutie_node = rospy.get_param("~cutie_node", "/cutie_node")
+        self.sam_node = rospy.get_param("~sam_node", "/sam_node")
+        self.tracking = rospy.get_param("~tracking", True)
 
         self.perception_query_service = rospy.Service(
             "/perception_reconfigure_node/reconfigure", PerceptionReconfigure, self.perception_reconfigure_service_cb
         )
+        if self.tracking:
+            self.reset_cutie = rospy.ServiceProxy("~reset_prompt", CutiePrompt)
 
     def perception_reconfigure_service_cb(self, req):
         detector = req.detector
@@ -26,12 +34,15 @@ class PerceptionReconfigureNode(object):
         if detector == "grounding_dino":
             self.gd_client = dynamic_reconfigure.client.Client(self.dino_node)
             self.gd_client.update_configuration({"classes": detection_query})
+            seg_topic = self.dino_node + "/output/segmentation"
         elif detector == "yolo":
             self.yolo_client = dynamic_reconfigure.client.Client(self.yolo_node)
             self.yolo_client.update_configuration({"classes": detection_query})
+            seg_topic = self.yolo_node + "/output/segmentation"
         elif detector == "vlpart":
             self.vlpart_client = dynamic_reconfigure.client.Client(self.vlpart_node)
             self.vlpart_client.update_configuration({"classes": detection_query})
+            seg_topic = self.vlpart_node + "/output/segmentation"
         elif detector == "vqa":
             self.vqa_client = dynamic_reconfigure.client.Client(self.vqa_node)
             self.vqa_client.update_configuration({"question": detection_query})
@@ -41,6 +52,16 @@ class PerceptionReconfigureNode(object):
         else:
             rospy.logerr("Invalid detector: {}".format(detector))
             return PerceptionReconfigureResponse(result=False)
+
+        if (detector == "grounding_dino" or detector == "yolo" or detector == "vlpart") and self.tracking:
+            input_img_msg = rospy.wait_for_message("~input_image", Image)
+            input_seg_msg = rospy.wait_for_message(seg_topic, Image)
+            rospy.wait_for_service("~reset_prompt")
+            try:
+                self.reset_cutie(CutiePromptRequest(image=input_img_msg, segmentation=input_seg_msg))
+            except rospy.ServiceException as e:
+                rospy.logerr("Service call failed: {}".format(e))
+                return PerceptionReconfigureResponse(result=False)
         return PerceptionReconfigureResponse(result=True)
 
 
